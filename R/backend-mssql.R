34936b1a3b6fccf37dc1979196c55ad2e5e185a1
David Fong <vkelim@bigpond.com>
2019-12-15 17:18:01 +1100

    restrict use of try_cast to version 11+ of MSSQL
    try_cast only available for MSSQL version 11+ (equivalent to 2012+)

--- R/backend-mssql.R
+++ R/backend-mssql.R
@@ -1,11 +1,11 @@
 #' @export
 `sql_select.Microsoft SQL Server` <- function(con, select, from, where = NULL,
-                                             group_by = NULL, having = NULL,
-                                             order_by = NULL,
-                                             limit = NULL,
-                                             distinct = FALSE,
-                                             ...,
-                                             bare_identifier_ok = FALSE) {
+                                              group_by = NULL, having = NULL,
+                                              order_by = NULL,
+                                              limit = NULL,
+                                              distinct = FALSE,
+                                              ...,
+                                              bare_identifier_ok = FALSE) {
   out <- vector("list", 7)
   names(out) <- c("select", "from", "where", "group_by",
                   "having", "order_by","limit")
@@ -47,127 +47,145 @@
 
 #' @export
 `sql_translate_env.Microsoft SQL Server` <- function(con) {
+
+  mssql_scalar <- sql_translator(.parent = base_odbc_scalar,
+
+                                 `!`           = function(x) {
+                                   if (sql_current_select()) {
+                                     build_sql(sql("~"), list(x))
+                                   } else {
+                                     sql_expr(NOT(!!x))
+                                   }
+                                 },
+
+                                 `!=`           = sql_infix("!="),
+                                 `==`           = sql_infix("="),
+                                 `<`            = sql_infix("<"),
+                                 `<=`           = sql_infix("<="),
+                                 `>`            = sql_infix(">"),
+                                 `>=`           = sql_infix(">="),
+
+                                 `&`            = mssql_generic_infix("&", "%AND%"),
+                                 `&&`           = mssql_generic_infix("&", "%AND%"),
+                                 `|`            = mssql_generic_infix("|", "%OR%"),
+                                 `||`           = mssql_generic_infix("|", "%OR%"),
+
+                                 bitwShiftL     = sql_not_supported("bitwShiftL"),
+                                 bitwShiftR     = sql_not_supported("bitwShiftR"),
+
+                                 `if`           = mssql_sql_if,
+                                 if_else        = function(condition, true, false) mssql_sql_if(condition, true, false),
+                                 ifelse         = function(test, yes, no) mssql_sql_if(test, yes, no),
+
+                                 as.logical    = sql_cast("BIT"),
+
+                                 as.Date       = sql_cast("DATE"),
+                                 as.numeric    = sql_cast("FLOAT"),
+                                 as.double     = sql_cast("FLOAT"),
+                                 as.character  = sql_cast("VARCHAR(MAX)"),
+                                 log           = sql_prefix("LOG"),
+                                 atan2         = sql_prefix("ATN2"),
+                                 ceil          = sql_prefix("CEILING"),
+                                 ceiling       = sql_prefix("CEILING"),
+
+                                 # https://dba.stackexchange.com/questions/187090
+                                 pmin          = sql_not_supported("pmin()"),
+                                 pmax          = sql_not_supported("pmax()"),
+
+                                 is.null       = function(x) mssql_is_null(x, sql_current_context()),
+                                 is.na         = function(x) mssql_is_null(x, sql_current_context()),
+
+                                 # string functions ------------------------------------------------
+                                 nchar = sql_prefix("LEN"),
+                                 paste = sql_paste_infix(" ", "+", function(x) sql_expr(cast(!!x %as% text))),
+                                 paste0 = sql_paste_infix("", "+", function(x) sql_expr(cast(!!x %as% text))),
+                                 substr = sql_substr("SUBSTRING"),
+
+                                 # stringr functions
+                                 str_length = sql_prefix("LEN"),
+                                 str_c = sql_paste_infix("", "+", function(x) sql_expr(cast(!!x %as% text))),
+                                 # no built in function: https://stackoverflow.com/questions/230138
+                                 str_to_title = sql_not_supported("str_to_title()"),
+                                 str_sub = sql_str_sub("SUBSTRING", "LEN"),
+
+                                 # lubridate ---------------------------------------------------------------
+                                 # https://en.wikibooks.org/wiki/SQL_Dialects_Reference/Functions_and_expressions/Date_and_time_functions
+                                 as_date = sql_cast("DATE"),
+
+                                 # Using DATETIME2 as it complies with ANSI and ISO.
+                                 # MS recommends DATETIME2 for new work:
+                                 # https://docs.microsoft.com/en-us/sql/t-sql/data-types/datetime-transact-sql?view=sql-server-2017
+                                 as_datetime = sql_cast("DATETIME2"),
+
+                                 today = function() sql_expr(CAST(SYSDATETIME() %AS% DATE)),
+
+                                 # https://docs.microsoft.com/en-us/sql/t-sql/functions/datepart-transact-sql?view=sql-server-2017
+                                 year = function(x) sql_expr(DATEPART(YEAR, !!x)),
+                                 day = function(x) sql_expr(DATEPART(DAY, !!x)),
+                                 mday = function(x) sql_expr(DATEPART(DAY, !!x)),
+                                 yday = function(x) sql_expr(DATEPART(DAYOFYEAR, !!x)),
+                                 hour = function(x) sql_expr(DATEPART(HOUR, !!x)),
+                                 minute = function(x) sql_expr(DATEPART(MINUTE, !!x)),
+                                 second = function(x) sql_expr(DATEPART(SECOND, !!x)),
+
+                                 month = function(x, label = FALSE, abbr = TRUE) {
+                                   if (!label) {
+                                     sql_expr(DATEPART(MONTH, !!x))
+                                   } else {
+                                     if (!abbr) {
+                                       sql_expr(DATENAME(MONTH, !!x))
+                                     } else {
+                                       stop("`abbr` is not supported in SQL Server translation", call. = FALSE)          }
+                                   }
+                                 },
+
+                                 quarter = function(x, with_year = FALSE, fiscal_start = 1) {
+                                   if (fiscal_start != 1) {
+                                     stop("`fiscal_start` is not supported in SQL Server translation. Must be 1.", call. = FALSE)
+                                   }
+
+                                   if (with_year) {
+                                     sql_expr((DATENAME(YEAR, !!x) + '.' + DATENAME(QUARTER, !!x)))
+                                   } else {
+                                     sql_expr(DATEPART(QUARTER, !!x))
+                                   }
+                                 },
+
+  )
+
+  if (sub("0.*", "", DBI::dbGetInfo(con)$db.version) >= 11) {
+    # version 11 equates to MSSQL 2012
+    mssql_scalar <- sql_translator(.parent = mssql_scalar,
+                                   as.logical    = sql_try_cast("BIT"),
+
+                                   as.Date       = sql_try_cast("DATE"),
+                                   as.POSIXct    = sql_try_cast("TIMESTAMP"),
+                                   as.numeric    = sql_try_cast("FLOAT"),
+                                   as.double     = sql_try_cast("FLOAT"),
+                                   as.integer    = sql_try_cast("NUMERIC"),
+                                   as.integer64  = sql_try_cast("BIGINT"),
+                                   as.character  = sql_try_cast("VARCHAR(MAX)"),
+
+                                   as_date = sql_try_cast("DATE"),
+                                   as_datetime = sql_try_cast("DATETIME2")
+    )
+  }
+
   sql_variant(
-    sql_translator(.parent = base_odbc_scalar,
-
-      `!`           = function(x) {
-                        if (sql_current_select()) {
-                          build_sql(sql("~"), list(x))
-                        } else {
-                          sql_expr(NOT(!!x))
-                        }
-                      },
-
-      `!=`           = sql_infix("!="),
-      `==`           = sql_infix("="),
-      `<`            = sql_infix("<"),
-      `<=`           = sql_infix("<="),
-      `>`            = sql_infix(">"),
-      `>=`           = sql_infix(">="),
-
-      `&`            = mssql_generic_infix("&", "%AND%"),
-      `&&`           = mssql_generic_infix("&", "%AND%"),
-      `|`            = mssql_generic_infix("|", "%OR%"),
-      `||`           = mssql_generic_infix("|", "%OR%"),
-
-      bitwShiftL     = sql_not_supported("bitwShiftL"),
-      bitwShiftR     = sql_not_supported("bitwShiftR"),
-
-      `if`           = mssql_sql_if,
-      if_else        = function(condition, true, false) mssql_sql_if(condition, true, false),
-      ifelse         = function(test, yes, no) mssql_sql_if(test, yes, no),
-
-      as.logical    = sql_try_cast("BIT"),
-
-      as.Date       = sql_try_cast("DATE"),
-      as.POSIXct    = sql_try_cast("TIMESTAMP"),
-      as.numeric    = sql_try_cast("FLOAT"),
-      as.double     = sql_try_cast("FLOAT"),
-      as.integer    = sql_try_cast("NUMERIC"), # in MSSQL this converts to integer
-      as.integer64  = sql_try_cast("BIGINT"),
-      as.character  = sql_try_cast("VARCHAR(MAX)"),
-      log           = sql_prefix("LOG"),
-      atan2         = sql_prefix("ATN2"),
-      ceil          = sql_prefix("CEILING"),
-      ceiling       = sql_prefix("CEILING"),
-
-      # https://dba.stackexchange.com/questions/187090
-      pmin          = sql_not_supported("pmin()"),
-      pmax          = sql_not_supported("pmax()"),
-
-      is.null       = function(x) mssql_is_null(x, sql_current_context()),
-      is.na         = function(x) mssql_is_null(x, sql_current_context()),
-
-      # string functions ------------------------------------------------
-      nchar = sql_prefix("LEN"),
-      paste = sql_paste_infix(" ", "+", function(x) sql_expr(cast(!!x %as% text))),
-      paste0 = sql_paste_infix("", "+", function(x) sql_expr(cast(!!x %as% text))),
-      substr = sql_substr("SUBSTRING"),
-
-      # stringr functions
-      str_length = sql_prefix("LEN"),
-      str_c = sql_paste_infix("", "+", function(x) sql_expr(cast(!!x %as% text))),
-      # no built in function: https://stackoverflow.com/questions/230138
-      str_to_title = sql_not_supported("str_to_title()"),
-      str_sub = sql_str_sub("SUBSTRING", "LEN"),
-
-      # lubridate ---------------------------------------------------------------
-      # https://en.wikibooks.org/wiki/SQL_Dialects_Reference/Functions_and_expressions/Date_and_time_functions
-      as_date = sql_try_cast("DATE"),
-
-      # Using DATETIME2 as it complies with ANSI and ISO.
-      # MS recommends DATETIME2 for new work:
-      # https://docs.microsoft.com/en-us/sql/t-sql/data-types/datetime-transact-sql?view=sql-server-2017
-      as_datetime = sql_try_cast("DATETIME2"),
-
-      today = function() sql_expr(CAST(SYSDATETIME() %AS% DATE)),
-
-      # https://docs.microsoft.com/en-us/sql/t-sql/functions/datepart-transact-sql?view=sql-server-2017
-      year = function(x) sql_expr(DATEPART(YEAR, !!x)),
-      day = function(x) sql_expr(DATEPART(DAY, !!x)),
-      mday = function(x) sql_expr(DATEPART(DAY, !!x)),
-      yday = function(x) sql_expr(DATEPART(DAYOFYEAR, !!x)),
-      hour = function(x) sql_expr(DATEPART(HOUR, !!x)),
-      minute = function(x) sql_expr(DATEPART(MINUTE, !!x)),
-      second = function(x) sql_expr(DATEPART(SECOND, !!x)),
-
-      month = function(x, label = FALSE, abbr = TRUE) {
-        if (!label) {
-          sql_expr(DATEPART(MONTH, !!x))
-        } else {
-          if (!abbr) {
-            sql_expr(DATENAME(MONTH, !!x))
-          } else {
-            stop("`abbr` is not supported in SQL Server translation", call. = FALSE)          }
-        }
-      },
-
-      quarter = function(x, with_year = FALSE, fiscal_start = 1) {
-        if (fiscal_start != 1) {
-          stop("`fiscal_start` is not supported in SQL Server translation. Must be 1.", call. = FALSE)
-        }
-
-        if (with_year) {
-          sql_expr((DATENAME(YEAR, !!x) + '.' + DATENAME(QUARTER, !!x)))
-        } else {
-          sql_expr(DATEPART(QUARTER, !!x))
-        }
-      },
-
-    ),
+    mssql_scalar,
     sql_translator(.parent = base_odbc_agg,
-      sd            = sql_aggregate("STDEV", "sd"),
-      var           = sql_aggregate("VAR", "var"),
-                      # MSSQL does not have function for: cor and cov
-      cor           = sql_not_supported("cor()"),
-      cov           = sql_not_supported("cov()")
+                   sd            = sql_aggregate("STDEV", "sd"),
+                   var           = sql_aggregate("VAR", "var"),
+                   # MSSQL does not have function for: cor and cov
+                   cor           = sql_not_supported("cor()"),
+                   cov           = sql_not_supported("cov()")
     ),
     sql_translator(.parent = base_odbc_win,
-      sd            = win_aggregate("STDEV"),
-      var           = win_aggregate("VAR"),
-      # MSSQL does not have function for: cor and cov
-      cor           = win_absent("cor"),
-      cov           = win_absent("cov")
+                   sd            = win_aggregate("STDEV"),
+                   var           = win_aggregate("VAR"),
+                   # MSSQL does not have function for: cor and cov
+                   cor           = win_absent("cor"),
+                   cov           = win_absent("cov")
     )
 
   )}
@@ -196,9 +214,9 @@ mssql_temp_name <- function(name, temporary){
 
 #' @export
 `db_copy_to.Microsoft SQL Server` <- function(con, table, values,
-                            overwrite = FALSE, types = NULL, temporary = TRUE,
-                            unique_indexes = NULL, indexes = NULL,
-                            analyze = TRUE, ...) {
+                                              overwrite = FALSE, types = NULL, temporary = TRUE,
+                                              unique_indexes = NULL, indexes = NULL,
+                                              analyze = TRUE, ...) {
   NextMethod(
     table = mssql_temp_name(table, temporary),
     types = types,
